184caa4c8bed84d1fbd84e92c304949f469df583
ruslan egorov <84963382+ruslaneg@users.noreply.github.com>
2024-03-12 12:38:40 +0300

    Fix today

--- src/util/dates.js
+++ src/util/dates.js
@@ -1,7 +1,9 @@
-const date = new Date();
+export const today = () => {
+  const date = new Date();
 
-export const today = () => ({
-  year: date.getFullYear(),
-  month: date.getMonth() + 1,
-  date: date.getDate(),
-});
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    date: date.getDate(),
+  };
+};
